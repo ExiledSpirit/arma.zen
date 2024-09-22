8a622eb9c01dabde4bb7b7cd0bec63e4b274a613
Exiled Spirit <gab.gabriel.1002@outlook.com>
2024-09-22 20:49:47 -0300

    feat: presentation & validations layert

---
+++ armazen/lib/validation/validators/required_field_validation.dart
@@ -0,0 +1,17 @@
+import 'package:armazen/presentation/protocols/validation.dart';
+import '../protocols/protocols.dart';
+import 'package:equatable/equatable.dart';
+
+class RequiredFieldValidation extends Equatable implements FieldValidation {
+  @override
+  final String field;
+
+  @override
+  List get props => [field];
+
+  const RequiredFieldValidation(this.field);
+
+  @override
+  ValidationError? validate(Map input) =>
+    input[field]?.isNotEmpty == true ? null : ValidationError.requiredField;
+}
